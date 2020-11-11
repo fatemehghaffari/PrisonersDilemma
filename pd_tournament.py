32b7f4c79834c6ce5e9bb786446e3a67f1f0d50b
Jesse <Jessegoodspeed@gmail.com>
2020-11-10 23:20:54 -0500

    Edited pd_tournament.py

--- pd_tournament.py
+++ pd_tournament.py
@@ -2,12 +2,11 @@ import axelrod as axl
 import matplotlib.pyplot as plt
 import pprint
 
-# Filter to extract all 32 memory-one strategies
+# Filter to extract all deterministic and memory-one strategies
 filterset = {
         'stochastic': False,
         'memory_depth': 1
         }
-
 
 strategies = axl.filtered_strategies(filterset)
 
@@ -15,6 +14,7 @@ strategies = axl.filtered_strategies(filterset)
 one_mem_players = [s() for s in strategies]
 print('Number of players: ', len(one_mem_players))
 
+# Instantiate tournament object
 tournament = axl.Tournament(
         players=one_mem_players,
         prob_end=0.1,
@@ -25,11 +25,15 @@ tournament = axl.Tournament(
 
 results = tournament.play()
 
+print('RANKED NAMES by Median Score: ')
 for name in results.ranked_names:
     print(name)
 
 print('Match lengths: ')
 pprint.pprint( results.match_lengths)
+
+for name, norm_score in zip(one_mem_players,results.normalised_scores):
+    print(name, ' | Normalized Score: ', norm_score)
 
 plot = axl.Plot(results)
 p = plot.payoff()
