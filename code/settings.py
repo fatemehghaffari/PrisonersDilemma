44e219ca24fea9b2e76ead9f8d9a9337bd9fbd8b
fatemehghaffari <47489341+fatemehghaffari@users.noreply.github.com>
2021-11-23 17:26:56 +0330

    Update settings.py

--- code/settings.py
+++ code/settings.py
@@ -1,5 +1,5 @@
 import axelrod as axl
-
+from axelrod.action import Action
 # Filter to extract all deterministic and memory-one strategies
 filterset = {
         'stochastic': False,
@@ -32,6 +32,38 @@ name_strategy_dict = dict()  # dictionary that maps string names to correspondin
 for name, player in zip(player_names, one_mem_players):
     name_strategy_dict[name]=player
 
+C,D = Action.C, Action.D
+action_map={0: D, 1: C}
+
+# Cooperators
+bitter_coop = axl.MemoryOnePlayer(tuple([1,0,1,1]),action_map[1])
+coop = axl.MemoryOnePlayer(tuple([1, 1, 1, 1]),action_map[1])
+fourteen_coop = axl.MemoryOnePlayer(tuple([1,1,1,0]),action_map[1])
+grim_trigger = axl.MemoryOnePlayer(tuple([1,0,0,0]),action_map[1])
+thirteen_coop = axl.MemoryOnePlayer(tuple([1,1,0,1]),action_map[1])
+tit_for_tat = axl.MemoryOnePlayer(tuple([1,0,1,0]),action_map[1])
+win_stay_lose_shift = axl.MemoryOnePlayer(tuple([1,0,0,1]),action_map[1])
+
+Coop_strategy_dict = {'Bitter Cooperator' : bitter_coop, 'Cooperator' : coop, 'Fourteen Coop' : fourteen_coop, 'Grim Trigger' : grim_trigger,
+              'Thirteen Coop' : thirteen_coop, 'Tit For Tat' : tit_for_tat, 'Win-Stay Lose-Shift' : win_stay_lose_shift}
+
+player_names = list(Coop_strategy_dict.keys())
+defector = axl.MemoryOnePlayer(tuple([0, 0, 0, 0]),action_map[0])
+fourteen_defect = axl.MemoryOnePlayer(tuple([1,1,1,0]),action_map[0])
+stubborn_defect = axl.MemoryOnePlayer(tuple([1,0,0,0]),action_map[0])
+suspicious_tit_for_tat = axl.MemoryOnePlayer(tuple([1,0,1,0]),action_map[0])
+sucker_defect = axl.MemoryOnePlayer(tuple([0,1,0,0]),action_map[0])
+two_defect = axl.MemoryOnePlayer(tuple([0,0,1,0]),action_map[0])
+win_shift_lose_stay = axl.MemoryOnePlayer(tuple([0,1,1,0]),action_map[0])
+def_strategy_dict = {'Defector' : defector, 'Fourteen Defect' : fourteen_defect, 'Stubborn Defect' : stubborn_defect, 
+            'Suspicious Tit For Tat' :  suspicious_tit_for_tat, 'Sucker Defect' : sucker_defect, 'Two Defect' : two_defect, 'Win-Shift Lose-Stay' : win_shift_lose_stay}
+player_names = list(def_strategy_dict.keys())
+
+CD_strategy_dict = {'Bitter Cooperator' : bitter_coop, 'Cooperator' : coop, 'Fourteen Coop' : fourteen_coop, 'Grim Trigger' : grim_trigger,
+              'Thirteen Coop' : thirteen_coop, 'Tit For Tat' : tit_for_tat, 'Win-Stay Lose-Shift' : win_stay_lose_shift, 'Defector' : defector,
+               'Fourteen Defect' : fourteen_defect, 'Stubborn Defect' : stubborn_defect, 'Suspicious Tit For Tat' :  suspicious_tit_for_tat, 
+               'Sucker Defect' : sucker_defect, 'Two Defect' : two_defect, 'Win-Shift Lose-Stay' : win_shift_lose_stay}
+player_names = list(CD_strategy_dict.keys())
 # Dictionary that maps string names to decimal
 name_dec_dict = dict()
 for num, name in enumerate(player_names,1):
